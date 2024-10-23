5d62c3baf677040d4317c7c6bd64b62fbea38fb7
Ross <ross.acheson@gmail.com>
2024-10-22 20:33:28 -0600

    Complete file upload tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FileUploadComponent } from './file-upload.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FileUploadComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'sistema-cid';
-}
+export class AppComponent { }
